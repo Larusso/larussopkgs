ee63efe846ef5ccd6803009ef1c744a24e33fa85
Manfred Endres <manfred.endres@tslarusso.de>
2020-03-21 19:08:25 +0100

    Add gdub

--- default.nix
+++ default.nix
@@ -1,13 +1,22 @@
 { system ? builtins.currentSystem }:
-let pkgs = import <nixpkgs> { inherit system; };
+let
+  pkgs = import <nixpkgs> { inherit system; };
+
+  stdenv = pkgs.stdenv;
+  fetchurl = pkgs.fetchurl;
+  appimageTools = pkgs.appimageTools;
+  gsettings-desktop-schemas = pkgs.gsettings-desktop-schemas;
+  gtk3 = pkgs.gtk3;
+  undmg = pkgs.undmg;
+  fetchFromGitHub = pkgs.fetchFromGitHub;
+  makeWrapper = pkgs.makeWrapper;
 
 in {
   unityhub = pkgs.callPackage ./pkgs/development/tools/unityhub {
-    stdenv = pkgs.stdenv;
-    fetchurl = pkgs.fetchurl;
-    appimageTools = pkgs.appimageTools;
-    gsettings-desktop-schemas = pkgs.gsettings-desktop-schemas;
-    gtk3 = pkgs.gtk3;
-    undmg = pkgs.undmg;
+    inherit stdenv fetchurl appimageTools gsettings-desktop-schemas gtk3 undmg;
+  };
+
+  gdub = pkgs.callPackage ./pkgs/development/tools/build-manager/gdub {
+    inherit stdenv fetchFromGitHub makeWrapper;
   };
 }
