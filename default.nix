ea316c0b2c8381e1dba716b8d55884b7ab7e51b2
Manfred Endres <manfred.endres@tslarusso.de>
2020-03-21 18:28:54 +0100

    Rename unityhub

--- default.nix
+++ default.nix
@@ -1,15 +1,13 @@
 { system ? builtins.currentSystem }:
-let
-  pkgs = import <nixpkgs> { inherit system; };
-  
-  self = rec {
-    unityhub = pkgs.callPackage ./pkgs/development/tools/unityhub {
-      stdenv = pkgs.stdenv;
-      fetchurl = pkgs.fetchurl;
-      appimageTools = pkgs.appimageTools;
-      gsettings-desktop-schemas = pkgs.gsettings-desktop-schemas;
-      gtk3 = pkgs.gtk3;
-      undmg = pkgs.undmg;
-    };
+let pkgs = import <nixpkgs> { inherit system; };
+
+in {
+  larusso.unityhub = pkgs.callPackage ./pkgs/development/tools/unityhub {
+    stdenv = pkgs.stdenv;
+    fetchurl = pkgs.fetchurl;
+    appimageTools = pkgs.appimageTools;
+    gsettings-desktop-schemas = pkgs.gsettings-desktop-schemas;
+    gtk3 = pkgs.gtk3;
+    undmg = pkgs.undmg;
   };
-in self
+}
